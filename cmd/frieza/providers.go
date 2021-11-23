b97d96778aa840adb11faff02448e576571a097a
Jérôme Jutteau <jerome.jutteau@outscale.com>
2021-11-23 15:55:11 +0100

    provider_example: hide from cli
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- cmd/frieza/providers.go
+++ cmd/frieza/providers.go
@@ -5,7 +5,6 @@ import (
 
 	. "github.com/outscale-dev/frieza/internal/common"
 	"github.com/outscale-dev/frieza/internal/providers/outscale_oapi"
-	"github.com/outscale-dev/frieza/internal/providers/provider_example"
 	"github.com/outscale-dev/frieza/internal/providers/s3"
 	"github.com/teris-io/cli"
 )
@@ -14,8 +13,8 @@ func ProviderNew(profile Profile) (Provider, error) {
 	switch profile.Provider {
 	case outscale_oapi.Name:
 		return outscale_oapi.New(profile.Config, Debug)
-	case provider_example.Name:
-		return provider_example.New(profile.Config, Debug)
+	//case provider_example.Name:
+	//	return provider_example.New(profile.Config, Debug)
 	case s3.Name:
 		return s3.New(profile.Config, Debug)
 	}
@@ -29,7 +28,7 @@ var providersCli = []func() (string, cli.Command){
 }
 
 var providersTypes = map[string][]ObjectType{
-	outscale_oapi.Name:    outscale_oapi.Types(),
-	provider_example.Name: provider_example.Types(),
-	s3.Name:               s3.Types(),
+	outscale_oapi.Name: outscale_oapi.Types(),
+	//provider_example.Name: provider_example.Types(),
+	s3.Name: s3.Types(),
 }
