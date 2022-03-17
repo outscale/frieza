647adc7f35ab376f740af8ed10e09e5d20adc1fb
Jérôme Jutteau <jerome.jutteau@outscale.com>
2022-03-17 10:42:00 +0100

    cli: add --json option
    Allow outputing json in conjunction with --auto-approve opion.
    Deletion logic has been moved to a specific class "Destroyer"
    
    closes #3
    
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- cmd/frieza/cli_nuke.go
+++ cmd/frieza/cli_nuke.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"log"
-	"os"
-	"strings"
 
 	. "github.com/outscale-dev/frieza/internal/common"
 	"github.com/teris-io/cli"
@@ -14,25 +11,29 @@ import (
 func cliNuke() cli.Command {
 	return cli.NewCommand("nuke", "delete ALL resources of specified profiles").
 		WithOption(cli.NewOption("plan", "Only show what resource would be deleted").WithType(cli.TypeBool)).
+		WithOption(cliJson()).
 		WithOption(cli.NewOption("auto-approve", "Approve resource deletion without confirmation").WithType(cli.TypeBool)).
 		WithOption(cliConfigPath()).
 		WithOption(cliDebug()).
 		WithArg(cli.NewArg("profile", "one or more profile").AsOptional()).
 		WithAction(func(args []string, options map[string]string) int {
 			setupDebug(options)
-			nuke(options["config"], args, options["plan"] == "true", options["auto-approve"] == "true")
+			nuke(options["config"], args, options["plan"] == "true", options["auto-approve"] == "true", options["json"] == "true")
 			return 0
 		})
 }
 
-func nuke(customConfigPath string, profiles []string, plan bool, autoApprove bool) {
+func nuke(customConfigPath string, profiles []string, plan bool, autoApprove bool, jsonOutput bool) {
+	if jsonOutput && !autoApprove {
+		cliFatalf(true, "Cannot use --json option without --auto-approve")
+	}
 	var configPath *string
 	if len(customConfigPath) > 0 {
 		configPath = &customConfigPath
 	}
 
 	if len(profiles) == 0 {
-		log.Fatalln("No profile provided, use --help for more details.")
+		cliFatalf(jsonOutput, "No profile provided, use --help for more details.")
 	}
 
 	uniqueProfiles := make(map[string]bool)
@@ -42,91 +43,34 @@ func nuke(customConfigPath string, profiles []string, plan bool, autoApprove boo
 
 	config, err := ConfigLoadWithDefault(configPath)
 	if err != nil {
-		log.Fatalf("Cannot load configuration: %s", err.Error())
+		cliFatalf(jsonOutput, "Cannot load configuration: %s", err.Error())
 	}
 
-	var providers []Provider
-	var objectsToDelete []Objects
-	objectsCount := 0
-
+	destroyer := NewDestroyer()
 	for profileName := range uniqueProfiles {
 		profile, err := config.GetProfile(profileName)
 		if err != nil {
-			log.Fatalf("Error while getting profile %s: %s", profileName, err.Error())
+			cliFatalf(jsonOutput, "Error while getting profile %s: %s", profileName, err.Error())
 		}
 		provider, err := ProviderNew(*profile)
 		if err != nil {
-			log.Fatalf("Error intializing profile %s: %s", profileName, err.Error())
+			cliFatalf(jsonOutput, "Error intializing profile %s: %s", profileName, err.Error())
 		}
-		toDelete := ReadObjects(&provider)
-		objectsCount += ObjectsCount(&toDelete)
-		fmt.Printf("Profile %s (%s):\n", profile.Name, provider.Name())
-		if objectsCount > 0 {
-			fmt.Print(ObjectsPrint(&provider, &toDelete))
-		} else {
-			fmt.Println("* no object *")
-		}
-		providers = append(providers, provider)
-		objectsToDelete = append(objectsToDelete, toDelete)
+		objectsToDelete := ReadObjects(&provider)
+		destroyer.add(profile, &provider, &objectsToDelete)
 	}
 
-	if objectsCount == 0 {
-		fmt.Println("\nNothing to delete, exiting")
-		return
-	}
-
+	destroyer.print(jsonOutput)
 	if plan {
 		return
 	}
-
+	if jsonOutput {
+		disableLogs()
+	}
 	message := fmt.Sprintf("Do you really want to delete ALL resources?\n" +
 		"  Frieza will delete all resources shown above.")
 	if !confirmAction(&message, autoApprove) {
 		log.Fatal("Nuke canceled")
 	}
-	loopDelete(providers, objectsToDelete)
-}
-
-func loopDelete(providers []Provider, objects []Objects) {
-	for {
-		var objectsCount []int
-		var totalObjectCount int
-		for i := range objects {
-			count := ObjectsCount(&objects[i])
-			totalObjectCount += count
-			objectsCount = append(objectsCount, count)
-		}
-		if totalObjectCount == 0 {
-			return
-		}
-		for i, provider := range providers {
-			if objectsCount[i] == 0 {
-				continue
-			}
-			DeleteObjects(&provider, objects[i])
-		}
-		for i, provider := range providers {
-			diff := NewDiff()
-			remaining := ReadNonEmptyObjects(&provider, objects[i])
-			diff.Build(&remaining, &objects[i])
-			objects[i] = diff.Retained
-		}
-	}
-}
-
-func confirmAction(message *string, autoApprove bool) bool {
-	if autoApprove {
-		return true
-	}
-	fmt.Printf("\n%s\n", *message)
-	fmt.Printf("  There is no undo. Only 'yes' will be accepted to confirm.\n\n")
-	fmt.Printf("  Enter a value: ")
-	reader := bufio.NewReader(os.Stdin)
-	response, _ := reader.ReadString('\n')
-	response = strings.Replace(response, "\n", "", -1)
-	response = strings.Replace(response, "\r", "", -1)
-	if response != "yes" {
-		return false
-	}
-	return true
+	destroyer.run()
 }
---
+++ cmd/frieza/destroyer.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	. "github.com/outscale-dev/frieza/internal/common"
+)
+
+type Destroyer struct {
+	Targets []DestroyerTarget `json:"targets"`
+}
+
+type DestroyerTarget struct {
+	profile     *Profile          `json:"-"`
+	JsonProfile *DestroyerProfile `json:"profile"`
+	provider    *Provider         `json:"-"`
+	Objects     *Objects          `json:"objects"`
+}
+
+type DestroyerProfile struct {
+	Name     string `json:"name"`
+	Provider string `json:"provider"`
+}
+
+func NewDestroyer() *Destroyer {
+	var destroyer Destroyer
+	return &destroyer
+}
+
+func (destroyer *Destroyer) add(profile *Profile, provider *Provider, objectsToDelete *Objects) {
+	target := DestroyerTarget{
+		profile: profile,
+		JsonProfile: &DestroyerProfile{
+			Name:     profile.Name,
+			Provider: profile.Provider,
+		},
+		provider: provider,
+		Objects:  objectsToDelete,
+	}
+	destroyer.Targets = append(destroyer.Targets, target)
+}
+
+func (destroyer *Destroyer) print(json bool) {
+	if json {
+		destroyer.print_json()
+	} else {
+		destroyer.print_human()
+	}
+}
+
+func (destroyer *Destroyer) print_human() {
+	count := len(destroyer.Targets)
+	totalObjectCount := 0
+	for i := 0; i < count; i++ {
+		target := destroyer.Targets[i]
+		fmt.Printf("Objects to delete in profile %s (%s):\n", target.profile.Name, (*target.provider).Name())
+		objectsCount := ObjectsCount(target.Objects)
+		if objectsCount == 0 {
+			fmt.Println("* no object *")
+		}
+		totalObjectCount += objectsCount
+		fmt.Print(ObjectsPrint(target.provider, target.Objects))
+	}
+	if totalObjectCount == 0 {
+		fmt.Println("\nNothing to delete")
+	}
+}
+
+func (destroyer *Destroyer) print_json() {
+	json_bytes, err := json.MarshalIndent(destroyer, "", "  ")
+	if err != nil {
+		cliFatalf(true, "Cannot serialize to json: %s", err.Error())
+	}
+	log.Print(string(json_bytes))
+}
+
+func (destroyer *Destroyer) run() {
+	var objects []*Objects
+	for i := range destroyer.Targets {
+		objects = append(objects, destroyer.Targets[i].Objects)
+	}
+	var hasObjectsLeft []bool
+	for range objects {
+		hasObjectsLeft = append(hasObjectsLeft, true)
+	}
+	for {
+		var objectsCount []int
+		var totalObjectCount int
+		for i := range objects {
+			objectsCount = append(objectsCount, 0)
+			if !hasObjectsLeft[i] {
+				continue
+			}
+			count := ObjectsCount(objects[i])
+			objectsCount[i] = count
+			if count == 0 {
+				hasObjectsLeft[i] = false
+				continue
+			}
+			totalObjectCount += count
+		}
+		if totalObjectCount == 0 {
+			return
+		}
+		for i, target := range destroyer.Targets {
+			if !hasObjectsLeft[i] {
+				continue
+			}
+			DeleteObjects(target.provider, *objects[i])
+		}
+		for i, target := range destroyer.Targets {
+			diff := NewDiff()
+			remaining := ReadNonEmptyObjects(target.provider, *objects[i])
+			diff.Build(&remaining, objects[i])
+			objects[i] = &diff.Retained
+		}
+	}
+}
+
+func confirmAction(message *string, autoApprove bool) bool {
+	if autoApprove {
+		return true
+	}
+	fmt.Printf("\n%s\n", *message)
+	fmt.Printf("  There is no undo. Only 'yes' will be accepted to confirm.\n\n")
+	fmt.Printf("  Enter a value: ")
+	reader := bufio.NewReader(os.Stdin)
+	response, _ := reader.ReadString('\n')
+	response = strings.Replace(response, "\n", "", -1)
+	response = strings.Replace(response, "\r", "", -1)
+	return response == "yes"
+}
