fc155dd49a084162a8d1a712a08f5736e3fc2052
Jérôme Jutteau <jerome.jutteau@outscale.com>
2022-05-24 11:18:14 +0200

    cli: add timeout option for nuke and clean commands
    closes #83
    
    Signed-off-by: Jérôme Jutteau <jerome.jutteau@outscale.com>

--- cmd/frieza/cli_nuke.go
+++ cmd/frieza/cli_nuke.go
@@ -11,6 +11,7 @@ import (
 func cliNuke() cli.Command {
 	return cli.NewCommand("nuke", "delete ALL resources of specified profiles").
 		WithOption(cli.NewOption("plan", "Only show what resource would be deleted").WithType(cli.TypeBool)).
+		WithOption(cli.NewOption("timeout", "Exit with error after a specific duration (ex: 30s, 5m, 1.5h)").WithType(cli.TypeString)).
 		WithOption(cliJson()).
 		WithOption(cli.NewOption("auto-approve", "Approve resource deletion without confirmation").WithType(cli.TypeBool)).
 		WithOption(cliConfigPath()).
@@ -18,12 +19,19 @@ func cliNuke() cli.Command {
 		WithArg(cli.NewArg("profile", "one or more profile").AsOptional()).
 		WithAction(func(args []string, options map[string]string) int {
 			setupDebug(options)
-			nuke(options["config"], args, options["plan"] == "true", options["auto-approve"] == "true", options["json"] == "true")
+			plan := options["plan"] == "true"
+			autoApprove := options["auto-approve"] == "true"
+			jsonOutput := options["json"] == "true"
+			timeout := "-1"
+			if len(options["timeout"]) > 0 {
+				timeout = options["timeout"]
+			}
+			nuke(options["config"], args, plan, autoApprove, jsonOutput, timeout)
 			return 0
 		})
 }
 
-func nuke(customConfigPath string, profiles []string, plan bool, autoApprove bool, jsonOutput bool) {
+func nuke(customConfigPath string, profiles []string, plan bool, autoApprove bool, jsonOutput bool, timeout string) {
 	if jsonOutput && !autoApprove {
 		cliFatalf(true, "Cannot use --json option without --auto-approve")
 	}
@@ -72,5 +80,6 @@ func nuke(customConfigPath string, profiles []string, plan bool, autoApprove boo
 	if !confirmAction(&message, autoApprove) {
 		log.Fatal("Nuke canceled")
 	}
+	go timeoutRunner(timeout, jsonOutput)
 	destroyer.run()
 }
