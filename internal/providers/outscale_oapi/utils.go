6ed5b32326135cf8d1e1b044d8deda6d9a2c08d6
Ch.-David Blot <charles-david.blot@outscale.com>
2025-11-24 17:31:20 +0100

    ✨ feat: sdk v3 and add OKS provider

--- internal/providers/outscale_oapi/utils.go
+++ internal/providers/outscale_oapi/utils.go
@@ -2,29 +2,24 @@ package outscale_oapi
 
 import (
 	"fmt"
-	"net/http"
 
-	osc "github.com/outscale/osc-sdk-go/v2"
+	"github.com/outscale/osc-sdk-go/v3/pkg/osc"
 )
 
 func extractApiError(err error) (bool, *osc.ErrorResponse) {
-	genericError, ok := err.(osc.GenericOpenAPIError)
+	genericError, ok := err.(*osc.ErrorResponse)
 	if ok {
-		errorsResponse, ok := genericError.Model().(osc.ErrorResponse)
-		if ok {
-			return true, &errorsResponse
-		}
-		return false, nil
+		return true, genericError
 	}
 	return false, nil
 }
 
-func getErrorInfo(err error, httpRes *http.Response) error {
+func getErrorInfo(err error) error {
 	if ok, apiError := extractApiError(err); ok {
-		return fmt.Errorf("%v %v %v %v", httpRes.Status, apiError.GetErrors()[0].GetCode(), apiError.GetErrors()[0].GetType(), apiError.GetErrors()[0].GetDetails())
-	}
-	if httpRes != nil {
-		return fmt.Errorf("http status %d", httpRes.Status)
+		return fmt.Errorf(
+			"%v",
+			apiError.GetCode(),
+		)
 	}
 
 	return err
